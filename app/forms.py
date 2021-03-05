d78b797d7c39db38ee7a584e2e9fa936a40a2fbc
katerijy <katerijy@yahoo.com>
2021-03-05 13:51:14 -0800

    forms

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,25 @@
-User
-# id (Primary Key)
-# first_name
-# last_name
-# username
-# phone number
-# email
-# password
-# billing address (if you want to get fancy)
-# shipping address (if you want to get fancy)
-# products (join row - connects to Product)
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, EqualTo, Email
 
-Product
-# id (Primary Key)
-# name
-# price
-# description
-# category
-# image (url)
+class CustomerInfo(FlaskForm):
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = IntegerField('Phone', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    shipping_address = TextAreaField('Shipping', validators=None)
+    billing_address = TextAreaField('Billing', validators=None) 
+    submit = SubmitField()
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField()
+    
+
+
+
