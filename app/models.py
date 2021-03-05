f7133bfd21f144d12ec6e732ebf9f7fa09a3a9c7
fjohngaughan <fjohngaughan@gmail.com>
2021-03-05 11:58:21 -0800

    first commit

---
+++ app/models.py
@@ -0,0 +1,20 @@
+
+User
+# id (Primary Key)
+# first_name
+# last_name
+# username
+# phone number
+# email
+# password
+# billing address (if you want to get fancy)
+# shipping address (if you want to get fancy)
+# products (join row - connects to Product)
+
+Product
+# id (Primary Key)
+# name
+# price
+# description
+# category
+# image (url)
---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+# basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__name__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'come on. not cool.'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.sendgrid.net'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'apikey'
+    MAIL_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
